f9309e00aabda38a32a143a6e83c5fb2edaffb07
frust4971 <19aj048@ms.denddai.ac.jp>
2021-03-12 07:53:25 +0900

    ページネーションの最初へ飛ぶリンクなどを削除（スマホで見ると崩れるため）

--- resources/views/vendor/pagination/original_pagination_view.blade.php
+++ resources/views/vendor/pagination/original_pagination_view.blade.php
@@ -1,16 +1,6 @@
 @if ($paginator->hasPages())
 
 <ul class="pagination" role="navigation">
-    {{-- First Page Link --}}
-    <!-- 最初のページへのリンク -->
-    <li class="page-item {{ $paginator->onFirstPage() ? ' disabled' : '' }}">
-        <a class="page-link" href="{{ $paginator->url(1) }}">&laquo;</a>
-    </li>
-    {{-- Previous Page Link --}}
-    <!-- 前のページへのリンク -->
-    <li class="page-item {{ $paginator->onFirstPage() ? ' disabled' : '' }}">
-        <a class="page-link" href="{{ $paginator->previousPageUrl() }}">&lsaquo;</a>
-    </li>
     {{-- Pagination Elemnts --}}
     
     {{-- Array Of Links --}}
@@ -30,15 +20,5 @@
             <li class="page-item"><a class="page-link" href="{{ $paginator->url($i) }}">{{ $i }}</a></li>
         @endif
     @endfor
-    {{-- Next Page Link --}}
-    <!-- 次のページへのリンク -->
-    <li class="page-item {{ $paginator->currentPage() == $end_page ? ' disabled' : '' }}">
-        <a class="page-link" href="{{ $paginator->nextPageUrl() }}">&rsaquo;</a>
-    </li>
-    {{-- Last Page Link --}}
-    <!-- 最後のページへのリンク -->
-    <li class="page-item {{ $paginator->currentPage() == $end_page ? ' disabled' : '' }}">
-        <a class="page-link" href="{{ $paginator->url($end_page) }}">&raquo;</a>
-    </li>
 </ul>
 @endif
