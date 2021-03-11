c7e67b57c36d574a5be9c8d1528631eea98424af
frust4971 <19aj048@ms.denddai.ac.jp>
2021-03-11 15:56:29 +0900

    メインクローラーのミスを修正

--- app/Python/CrawlerMain/CrawlerMain.py
+++ app/Python/CrawlerMain/CrawlerMain.py
@@ -8,10 +8,10 @@ import Crawler
 #データベースを新規作成後実行するクラス
 if __name__ == "__main__":
     #最近話題のボカロを集める
-    recently_vocalo_published_after = (datetime.datetime.today() + relativedelta(months=-12)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    recently_vocalo_published_after = (datetime.datetime.today() + relativedelta(months=-24)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
     recently_vocalo_published_before = (datetime.datetime.today()).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-    Crawler.crawlAndInsertToDB("recently_famous_vocalovideos", "ボカロ 歌ってみた", True, "short", 100000, recently_vocalo_published_after, recently_vocalo_published_before)
-    Crawler.crawlAndInsertToDB("recently_famous_vocalovideos","ボカロ 歌ってみた",True,"medium",100000,recently_vocalo_published_after,recently_vocalo_published_before)
+    Crawler.crawlAndInsertToDB("recently_famous_vocalovideos", "ボカロ", True, "short", 100000, recently_vocalo_published_after, recently_vocalo_published_before)
+    Crawler.crawlAndInsertToDB("recently_famous_vocalovideos","ボカロ",True,"medium",100000,recently_vocalo_published_after,recently_vocalo_published_before)
     
     #最近話題の歌ってみたを集める
     recently_utattemita_published_after = (datetime.datetime.today() + relativedelta(months=-13)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
