dd3cbb53fa29a167c5497cbdbc50c6b5deadd58b
frust4971 <19aj048@ms.denddai.ac.jp>
2021-03-12 19:53:08 +0900

    キャッシュ取らないほうが早い説。整合性を高めるためにとらないことにした

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,21 +9,8 @@ use Illuminate\Support\Facades\DB;
 class HomeController extends Controller
 {
     public function index(){ 
-        //1日の秒数
-        $ttl=86400;
-        if(Cache::has('pickup_vocalovideos_cache')){
-            $pickup_vocalovideos = Cache::get('pickup_vocalovideos_cache');
-        }else{
-            $pickup_vocalovideos = DB::table('recently_famous_vocalovideos')->orderBy('published_at','desc')->limit(9)->get();
-            Cache::put('pickup_vocalovideos_cache',$pickup_vocalovideos,$ttl);
-        }
-        if(Cache::has('pickup_utattemita')){
-            $pickup_utattemita = Cache::get('pickup_utattemita');
-        }else{
-            $pickup_utattemita = DB::table('recently_famous_utattemita')->orderBy('published_at','desc')->limit(9)->get();
-            Cache::put('pickup_vocalovideos_cache',$pickup_vocalovideos,$ttl);
-        }
-
+        $pickup_vocalovideos = DB::table('recently_famous_vocalovideos')->orderBy('published_at','desc')->limit(9)->get();
+        $pickup_utattemita = DB::table('recently_famous_utattemita')->orderBy('published_at','desc')->limit(9)->get();
         return view('home',compact('pickup_vocalovideos','pickup_utattemita'));
     }
 }
