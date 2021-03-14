6d749f617c7b91d9db2f66bd23f017f2ececd943
frust4971 <19aj048@ms.denddai.ac.jp>
2021-03-14 09:31:04 +0900

    最終は最終じゃなかった。メインクローラーの修正

--- app/Python/CrawlerMain/CrawlerMain.py
+++ app/Python/CrawlerMain/CrawlerMain.py
@@ -11,8 +11,11 @@ if __name__ == "__main__":
     #最近話題のボカロを集める
     recently_vocalo_published_after = (datetime.datetime.today() + relativedelta(months=-12)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
     recently_vocalo_published_before = (datetime.datetime.today()).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-    Crawler.crawlAndInsertToDB("recently_famous_vocalovideos", "ボカロ", False, "short", 100000, recently_vocalo_published_after, recently_vocalo_published_before)
-    Crawler.crawlAndInsertToDB("recently_famous_vocalovideos","ボカロ",False,"medium",100000,recently_vocalo_published_after,recently_vocalo_published_before)
+    researchWords = ["vocaloid", "ボカロ", "ボカロ feat", "ボカロ GUMI", "ボカロ IA", "ボカロ　ミク", "ボカロ　リン", "ボカロ　レン", "ボカロ　ウナ",
+    "ボカロ　ルカ","ボカロ　flower","ボカロ Lily","ボカロ　KAITO","ボカロ　miki","ボカロ　mayu"]
+    for researchWord in researchWords:
+        Crawler.crawlAndInsertToDB("recently_famous_vocalovideos", researchWord, False, "short", 100000, recently_vocalo_published_after, recently_vocalo_published_before)
+        Crawler.crawlAndInsertToDB("recently_famous_vocalovideos",researchWord,False,"medium",100000,recently_vocalo_published_after,recently_vocalo_published_before)
     
     #最近話題の歌ってみたを集める
     recently_utattemita_published_after = (datetime.datetime.today() + relativedelta(months=-12)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
