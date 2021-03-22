225b3d79266881c2b7130a56a1c91099eef3681a
frust4971 <19aj048@ms.denddai.ac.jp>
2021-03-23 07:44:57 +0900

    一か月に変更

--- app/Python/CrawlerMain/crawler_main.py
+++ app/Python/CrawlerMain/crawler_main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         crawler.crawl_and_insert_into_db("recently_famous_vocalovideos",researchWord,False,"medium",100000,recently_vocalo_published_after,recently_vocalo_published_before)
     
     #最近話題の歌ってみたを集める
-    recently_utattemita_published_after = (datetime.datetime.today() + relativedelta(months=-3)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    recently_utattemita_published_after = (datetime.datetime.today() + relativedelta(months=-1)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
     recently_utattemita_published_before = (datetime.datetime.today()).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
     crawler.crawl_and_insert_into_db("recently_famous_utattemita", "ボカロ 歌ってみた", True, "short", 100000, recently_utattemita_published_after, recently_utattemita_published_before)
     crawler.crawl_and_insert_into_db("recently_famous_utattemita", "ボカロ 歌ってみた", True, "medium", 100000, recently_utattemita_published_after, recently_utattemita_published_before)
