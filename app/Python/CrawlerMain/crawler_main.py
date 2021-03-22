3d9d14544d20677694a146e1015dd5283a98a3d1
frust4971 <19aj048@ms.denddai.ac.jp>
2021-03-23 07:33:26 +0900

    歌ってみたの収集期間を三か月に変更。要調整

--- app/Python/CrawlerMain/crawler_main.py
+++ app/Python/CrawlerMain/crawler_main.py
@@ -9,16 +9,16 @@ import crawler
 #データベースを新規作成後実行するクラス
 if __name__ == "__main__":
     #最近話題のボカロを集める
-    recently_vocalo_published_after = (datetime.datetime.today() + relativedelta(months=-6)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-    recently_vocalo_published_before = (datetime.datetime.today()).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-    researchWords = ["vocaloid", "ボカロ", "ボカロ feat", "ボカロ GUMI", "ボカロ IA", "ボカロ　ミク", "ボカロ　リン", "ボカロ　レン", "ボカロ　ウナ",
-    "ボカロ　ルカ","ボカロ　flower","ボカロ Lily","ボカロ　KAITO","ボカロ　miki","ボカロ　mayu"]
-    for researchWord in researchWords:
-        crawler.crawl_and_insert_into_db("recently_famous_vocalovideos", researchWord, False, "short", 100000, recently_vocalo_published_after, recently_vocalo_published_before)
-        crawler.crawl_and_insert_into_db("recently_famous_vocalovideos",researchWord,False,"medium",100000,recently_vocalo_published_after,recently_vocalo_published_before)
+    # recently_vocalo_published_after = (datetime.datetime.today() + relativedelta(months=-6)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    # recently_vocalo_published_before = (datetime.datetime.today()).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    # researchWords = ["vocaloid", "ボカロ", "ボカロ feat", "ボカロ GUMI", "ボカロ IA", "ボカロ　ミク", "ボカロ　リン", "ボカロ　レン", "ボカロ　ウナ",
+    # "ボカロ　ルカ","ボカロ　flower","ボカロ Lily","ボカロ　KAITO","ボカロ　miki","ボカロ　mayu"]
+    # for researchWord in researchWords:
+    #     crawler.crawl_and_insert_into_db("recently_famous_vocalovideos", researchWord, False, "short", 100000, recently_vocalo_published_after, recently_vocalo_published_before)
+    #     crawler.crawl_and_insert_into_db("recently_famous_vocalovideos",researchWord,False,"medium",100000,recently_vocalo_published_after,recently_vocalo_published_before)
     
     #最近話題の歌ってみたを集める
-    recently_utattemita_published_after = (datetime.datetime.today() + relativedelta(months=-6)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    recently_utattemita_published_after = (datetime.datetime.today() + relativedelta(months=-3)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
     recently_utattemita_published_before = (datetime.datetime.today()).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
     crawler.crawl_and_insert_into_db("recently_famous_utattemita", "ボカロ 歌ってみた", True, "short", 100000, recently_utattemita_published_after, recently_utattemita_published_before)
     crawler.crawl_and_insert_into_db("recently_famous_utattemita", "ボカロ 歌ってみた", True, "medium", 100000, recently_utattemita_published_after, recently_utattemita_published_before)
