961c22c996c1855936f9d4474c19ef8dabb687ca
frust4971 <19aj048@ms.denddai.ac.jp>
2021-03-24 16:42:59 +0900

    修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,7 +30,7 @@
             @for($i = 0; $i < 3; $i++)
                 <div class="carousel-item {{$i == 0 ? 'active' : NULL}}">
                     <div class="display-if-lg-or-more">
-                        <div class="row d-none d-lg-flex">
+                        <div class="d-none d-lg-flex">
                         @for($j = $i * 3; $j < ($i + 1) * 3; $j++)
                                 <div class="embed-responsive embed-responsive-16by9 carousel-grid col-4">
                                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{{$pickup_vocalovideos[$j]->video_id}}"></iframe>
@@ -39,7 +39,7 @@
                         </div>
                     </div>
                     <div class="display-if-under-lg">
-                        <div class="row d-lg-none d-block">
+                        <div class="d-lg-none d-block">
                             <div class="embed-responsive embed-responsive-16by9 carousel-grid col-12">
                                 <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{{$pickup_vocalovideos[$i]->video_id}}"></iframe>
                             </div>
