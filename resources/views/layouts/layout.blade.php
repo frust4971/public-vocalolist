14cb3e5861efeb6e5373d335f849c5e97588f8c4
frust4971 <19aj048@ms.denddai.ac.jp>
2021-10-01 11:17:49 +0900

    スマホで再生回数が電話番号として表示されないよう修正

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -21,6 +21,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <meta name="format-detection" content="telephone=no">
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
