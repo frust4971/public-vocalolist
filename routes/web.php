8e39457db5b42f87dbeadf11b5914799a86b974e
frust4971 <19aj048@ms.denddai.ac.jp>
2021-10-01 13:46:04 +0900

    /と/homeのパスがおんなじ

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@
 |
 */
 
-Route::get('/', 'HomeController@index');
+Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/home','HomeController@index')->name('home');
 Route::get('/info/about_site',function(){
     return view('info.about_site');
 })->name('info.about_site');
